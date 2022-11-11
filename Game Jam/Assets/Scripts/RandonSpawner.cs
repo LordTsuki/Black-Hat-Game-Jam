4e97f8ec635cb0bad67db195b7a236f513111e53
Lord Tsuki <xyzgames@outlook.com.br>
2022-11-11 18:09:07 -0300

    Random Enemy Spawn V.1

--- Game Jam/Assets/Scripts/RandonSpawner.cs
+++ Game Jam/Assets/Scripts/RandonSpawner.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class RandonSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform[] spawnPoints;
+    public GameObject[] enemyPrefabs;
+
     void Start()
     {
         
@@ -13,6 +15,12 @@ public class RandonSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            int randEnemy = Random.Range(0, enemyPrefabs.Length);
+            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(enemyPrefabs[randEnemy], spawnPoints[randSpawnPoint].position, transform.rotation);
+        }    
     }
 }
